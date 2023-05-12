91bd1a7e990e43eb09e559714d1de0986dfca8eb
Mykhailo <misha.vrana@gmail.com>
2023-05-12 12:23:38 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Misha Vrana on 12.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
